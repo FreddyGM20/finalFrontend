3bfbb01d9701b3f85c6b23b439457f9ba0ccc555
FreddyGM20 <fguete@uninorte.edu.co>
2023-11-28 18:18:27 -0500

    funcion de formulario login (tecla enter)

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,8 +1,7 @@
-// src/components/Login.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import users from '../../lists/users';
-import style from './login.module.css'; // Importa los estilos
+import style from './login.module.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -23,37 +22,46 @@ const Login = () => {
     }
   };
 
+  const handleKeyPress = (e) => {
+    // Si la tecla presionada es "Enter", llama a handleLogin
+    if (e.key === 'Enter') {
+      handleLogin();
+    }
+  };
+
   // Agrega o quita la clase login-body al body según la página actual
   document.body.classList.add('login-body');
   // Limpia la clase cuando el componente se desmonta
   return (
     <div className={style.principalContainer}>
-    <div className={style.loginContainer}>
-      <h2 className={style.loginTitle}>Login</h2>
-      <label className={style.loginLabel}>
-        Username:
-        <input
-          className={style.loginInput}
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <br />
-      <label className={style.loginLabel}>
-        Password:
-        <input
-          className={style.loginInput}
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button className={style.loginButton} onClick={handleLogin}>
-        Login
-      </button>
-    </div>
+      <div className={style.loginContainer}>
+        <h2 className={style.loginTitle}>Login</h2>
+        <label className={style.loginLabel}>
+          Username:
+          <input
+            className={style.loginInput}
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+        </label>
+        <br />
+        <label className={style.loginLabel}>
+          Password:
+          <input
+            className={style.loginInput}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+        </label>
+        <br />
+        <button className={style.loginButton} onClick={handleLogin}>
+          Login
+        </button>
+      </div>
     </div>
   );
 };
